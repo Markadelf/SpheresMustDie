838c31b3c2a4551529cd15a6f17582d5a0afec3e
Mark DelFavero <markadelf@gmail.com>
2019-04-14 14:08:14 -0400

    Added particles on destruction of objects

---
+++ Assets/Scripts/Environmental/DeathPoof.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPoof : MonoBehaviour {
+
+    private ParticleSystem particle;
+
+	// Use this for initialization
+	void Awake () {
+        particle = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!particle.isEmitting)
+        {
+            ObjectPool.Release(gameObject);
+        }
+	}
+
+    private void OnEnable()
+    {
+        particle.Play();
+    }
+}
---
+++ Assets/Scripts/Environmental/PoofOnDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoofOnDeath : MonoBehaviour {
+    public GameObject poofPrefab = null;
+
+    private void OnDestroy()
+    {
+        GameObject poof = ObjectPool.GetObj(poofPrefab);
+        if (poof != null)
+        {
+            poof.transform.position = transform.position;
+            poof.SetActive(true);
+            ParticleSystem.MainModule main = poof.GetComponent<ParticleSystem>().main;
+            main.startColor = GetComponent<Renderer>().material.color;
+        }
+    }
+}
