0de960aab17d3c35384a6e28888786acee496087
Mark DelFavero <markadelf@gmail.com>
2019-04-16 13:10:06 -0400

    Boss tracking system

---
+++ Assets/Scripts/Environmental/DeadBossSwitch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadBossSwitch : MonoBehaviour {
+
+    public int bossId;
+    public GameObject alive;
+    public GameObject dead;
+
+    public bool checkEachFrame;
+
+	// Use this for initialization
+	void Start () {
+        if (BossDeathTracker.CheckDead(bossId))
+        {
+            alive.SetActive(false);
+            dead.SetActive(true);
+        }
+        else
+        {
+            alive.SetActive(true);
+            dead.SetActive(false);
+        }
+        if(!checkEachFrame)
+        {
+            Destroy(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (BossDeathTracker.CheckDead(bossId))
+        {
+            alive.SetActive(false);
+            dead.SetActive(true);
+        }
+        else
+        {
+            alive.SetActive(true);
+            dead.SetActive(false);
+        }
+    }
+}
