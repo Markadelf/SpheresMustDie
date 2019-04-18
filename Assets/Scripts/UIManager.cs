95a1bdf796c3f1d7395f8ad8ade6b9cedd15c7e9
Ollyting Xin (RIT Student) <ox7313@ad.rit.edu>
2019-04-18 13:31:28 -0400

    add add boss

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -26,10 +26,10 @@ public class UIManager : MonoBehaviour {
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            SceneManager.LoadScene("LaunchScene");
-        }
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    SceneManager.LoadScene("LaunchScene");
+        //}
     }
 
 
