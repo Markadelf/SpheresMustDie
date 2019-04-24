6e0b5ae602b1ed3e05b3428c27978bbdbeee0682
Mark DelFavero <markadelf@gmail.com>
2019-04-24 10:02:55 -0400

    Worm boss is harder to kite

--- Assets/Scripts/Enemy/FlierAI.cs
+++ Assets/Scripts/Enemy/FlierAI.cs
@@ -7,6 +7,8 @@ public class FlierAI : MonoBehaviour {
     public float Agility;
     public float SightDistance = -1;
     public LayerMask BlockSight = 1;
+    public float YWeight = 1;
+    public float angleLimit = 90; // between 0 and 90
 
     private Rigidbody rigid;
     private Gun blaster;
@@ -26,9 +28,10 @@ public class FlierAI : MonoBehaviour {
         if (FirstPerson.PLAYER)
         {
             Vector3 toPlayer = FirstPerson.PLAYER.transform.position - transform.position;
+            toPlayer.y *= YWeight;
             if (alert)
             {
-                transform.forward = Vector3.RotateTowards(transform.forward, toPlayer, Agility * Time.deltaTime, 10000);
+                transform.forward = Vector3.RotateTowards(Vector3.down, Vector3.RotateTowards(Vector3.up, Vector3.RotateTowards(transform.forward, toPlayer, Agility * Time.deltaTime, 10000), angleLimit + 90, 1000), angleLimit + 90, 1000);
                 rigid.velocity = transform.forward * Speed;
                 if (blaster)
                 {
