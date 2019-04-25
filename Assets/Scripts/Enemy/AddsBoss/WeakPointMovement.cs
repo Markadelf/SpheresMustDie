21a501a88764b6228067e1d2ce05c74679ff5a41
OllytingXin <42716913+OllytingXin@users.noreply.github.com>
2019-04-25 01:57:28 -0400

    adjust the brightness of each scene

---
+++ Assets/Scripts/Enemy/AddsBoss/WeakPointMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakPointMovement : MonoBehaviour {
+
+    public float speed;
+    public GameObject centerObj;
+	// Use this for initialization
+	void Start () {
+        if (!FirstPerson.EASY_MODO)
+        {
+            this.transform.position = this.transform.parent.position + new Vector3(0, 3, 1);
+        }
+        else if (FirstPerson.EASY_MODO) {
+            this.transform.position = this.transform.parent.position + new Vector3(0, 0.2f, 2);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!FirstPerson.EASY_MODO)
+        {
+            this.transform.RotateAround(centerObj.transform.position, centerObj.transform.forward, speed * Time.deltaTime);
+        }      
+	}
+}
